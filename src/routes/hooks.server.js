54fda93602671f04ac1268626b7b30c61d04413b
dwiki <dwikikautsarfahmist@gmail.com>
2025-02-23 12:23:45 +0700

    fixed file hooks.server.js yang hilang

---
+++ src/routes/hooks.server.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import connectDB from '$lib/server/database';
+
+export const handle = async ({ event, resolve }) => {
+	try {
+		await connectDB(); // Panggil connectDB di sini
+	} catch (error) {
+		// Tangani error koneksi jika diperlukan
+		console.error('Gagal terhubung ke database:', error);
+		// Mungkin kirim response error atau tampilkan pesan kesalahan
+		return new Response('Terjadi kesalahan server', { status: 500 });
+	}
+
+	const response = await resolve(event);
+	return response;
+};
